03de4ad5c6239facf58c18a519230041a2d5c3c6
simonenkof <simonenko.f.a@gmail.com>
2024-10-30 20:01:33 +0300

    migrate from vite to webpack

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,7 @@
-export default {
+import type { Config } from "jest";
+
+const config: Config = {
   preset: "ts-jest",
-  testEnvironment: "jsdom",
 };
+
+export default config;
---
+++ webpack.config.js
@@ -0,0 +1,88 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/index.tsx"),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(ts)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        exclude: /\.module\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.module\.css$/i,
+        exclude: /node_modules/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(jpg|jpeg|png|svg)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  resolve: {
+    extensions: [
+      "*",
+      ".js",
+      ".jsx",
+      ".ts",
+      ".tsx",
+      ".json",
+      ".css",
+      ".scss",
+      ".png",
+      ".svg",
+      ".jpg",
+    ],
+    alias: {
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@ui": path.resolve(__dirname, "./src/components/ui"),
+      "@ui-pages": path.resolve(__dirname, "./src/components/ui/pages"),
+      "@utils-types": path.resolve(__dirname, "./src/utils/types"),
+      "@api": path.resolve(__dirname, "./src/utils/burger-api.ts"),
+      "@slices": path.resolve(__dirname, "./src/services/slices"),
+      "@selectors": path.resolve(__dirname, "./src/services/selectors"),
+    },
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: path.join(__dirname, "./dist"),
+    compress: true,
+    historyApiFallback: true,
+    port: 4000,
+  },
+};
